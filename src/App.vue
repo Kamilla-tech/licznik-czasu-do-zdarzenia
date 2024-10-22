030960e96d73d76ac2f27d0a11fc0415151466c6
Kamila Rembisz-Koszkul <kamila.rembisz@gmail.com>
2024-10-22 12:30:57 +0200

    import w script komponnetów CountdownForm oraz CountdownList.vue z vue. zwrócenie w script addCountdown oraz deleteCountDown. Dodanie styli w style.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <CountdownForm @add-countdown="addCountdown"/>
+    
+    <CountdownList :countdowns="countdowns" @delete-countdown="deleteCountdown"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import { ref } from 'vue';
+    
+    import CountdownForm from "./components/CountdownForm.vue"
+    import CountdownList from "./components/CountdownList.vue"
+    
+    export default {
+      components: {
+        CountdownForm,
+        CountdownList
+      },
+      setup() {
+        const countdowns = ref([]);
+        
+        const addCountdown = (countdown) => {
+          countdowns.value.push(countdown);
+        }
+        
+        const deleteCountdown = (id) => {
+          const index = countdowns.value.findIndex(c => c.id === id);
+          if (index !== -1) countdowns.value.splice(index, 1);
+        }
+        
+        return {
+          countdowns,
+          addCountdown,
+          deleteCountdown
+        }
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border-radius: 12px;
+    background: rgba(255, 255, 255, 0.9);
+    color: black;
+  }
 </style>
