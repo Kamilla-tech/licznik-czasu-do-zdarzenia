cca94f99a5f7d0926853b7c5c83efc510ca92124
Kamila Rembisz-Koszkul <kamila.rembisz@gmail.com>
2024-10-23 12:21:52 +0200

    utworzenie komponentu CountdownList i dodanie do niego script w którym importuje CountdownList z vue. Zwrocenie w Script deletCountdown. Dodanie styli w style. Utworzenie w template div z klasą countdowns-container.

---
+++ src/components/CountdownList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="countdowns-container">
+    <CountdownItem
+        v-for="countdown in countdowns"
+        :key="countdown.id"
+        :countdown="countdown"
+        @delete="deleteCountdown(countdown.id)"
+    />
+
+  </div>
+</template>
+
+<script>
+  import CountdownItem from "./CountdownItem.vue"
+  
+  export default {
+    components: {
+      CountdownItem
+    },
+    props: {
+      countdowns: Array
+    },
+    setup(_, { emit }) {
+      const deleteCountdown = (id) => {
+        emit("delete-countdown", id);
+      }
+      
+      return {
+        deleteCountdown
+      }
+    }
+  }
+</script>
+
+<style>
+  .countdowns-container {
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    margin-bottom: 20px;
+    text-align: center;
+  }
+</style>
