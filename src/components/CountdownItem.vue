e91cdc736b52d92800e0303ae198356cc648ac03
Kamila Rembisz-Koszkul <kamila.rembisz@gmail.com>
2024-10-24 15:20:44 +0200

    Dodanie styli w style. utowrzenie w template div z klasą countdown-finished oraz countdown-body. utowrzenie timer w ul oraz doadnie klas w li span days, hours, minutes oraz seconds. Dodanie div z v-else o zakończenkiu odliczania oraz dodanie przycisku countdown-delete.

--- src/components/CountdownItem.vue
+++ src/components/CountdownItem.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-
+  <div :class="{ 'countdown-finished': countdown.finished, 'countdown': true}">
+    <h3 class="countdown-title">{{ countdown.title }}</h3>
+    <div class="countdown-body" v-if="!countdown.finished">
+      <ul class="timer">
+        <li><span class="days"> {{ countdown.days }}</span>Dni</li>
+        <li><span class="hours"> {{ countdown.hours }}</span>Godzin</li>
+        <li><span class="minutes"> {{ countdown.minutes }}</span>Minut</li>
+        <li><span class="seconds"> {{ countdown.seconds }}</span>Sekund</li>
+      </ul>
+    </div>
+    <div v-else class="countdown-finished-info">
+      Odliczanie zakończone!
+    </div>
+    <button class="countdown-delete" @click="deleteCountdown">X</button>
   </div>
 </template>
 
@@ -52,5 +64,76 @@
 </script>
 
 <style>
+  .countdown {
+    position: relative;
+    margin-top: 5px;
+    padding: 5px;
+  }
   
+  .countdown-title {
+    margin: 0;
+    padding: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  
+  .countdown-body {
+    display: flex;
+    justify-content: center;
+    border-bottom: 1px dotted black;
+  }
+  
+  .countdown-finished {
+    background-color: #02aab0;
+    border-radius: 5px;
+    color: white;
+  }
+  
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  
+  ul li {
+    display: inline-block;
+    padding: 5px;
+    font-size: 1rem;
+    text-transform: uppercase;
+    list-style-type: none;
+  }
+  
+  ul li span {
+    display: block;
+    text-align: center;
+  }
+  
+  .timer.hide {
+    display: none;
+  }
+  
+  .countdown-finished-info {
+    font-weight: bold;
+  }
+  
+  .countdown-finished-info.show {
+    display: block;
+  }
+  
+  button.countdown-delete {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    display: block;
+    height: 20px;
+    background-color: inherit;
+    cursor: pointer;
+    border: none;
+    border-radius: 4px;
+    font-weight: bold;
+  }
+
+  button.countdown-delete:hover {
+    background-color: red;
+  }
 </style>
